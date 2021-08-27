bea6da07b8e51823059b02ea8b18fe43739a8ea5
sugar-shay <72109721+sugar-shay@users.noreply.github.com>
2021-08-27 14:07:24 -0500

    Add files via upload

--- active_learning.py
+++ active_learning.py
@@ -110,11 +110,11 @@ def main():
         save_file = 'bert_'+str(len(init_train))
         
         
-        model = LIT_SNLI(num_classes = 3, hidden_dropout_prob=.1, attention_probs_dropout_prob=.1, encoder_name=encoder_name, save_fp = save_dir+'/'+save_file+'.pt')
+        model = LIT_SNLI(num_classes = 3, hidden_dropout_prob=.1, attention_probs_dropout_prob=.1, encoder_name=encoder_name, save_fp = save_dir+'/bert_train.pt')
         model = train_LitModel(model, train_data, val_data, max_epochs=10, batch_size=4, patience = 3, num_gpu=4)
         
         model = LIT_SNLI(num_classes = 3, hidden_dropout_prob=.1, attention_probs_dropout_prob=.1, encoder_name=encoder_name)
-        model.load_state_dict(torch.load(save_dir+'/'+save_file+'.pt'))
+        model.load_state_dict(torch.load(save_dir+'/bert_train.pt'))
         
         cr = model_testing(model, test_data)
         
